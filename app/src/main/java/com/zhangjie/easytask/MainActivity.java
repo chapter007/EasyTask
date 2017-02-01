d28703b83798c1fb3bb66c2ce69c020b5cf552ab
zhangjie <zx.zhangjie@qq.com>
2017-02-01 20:27:35 +0800

    remove useless code

--- app/src/main/java/com/zhangjie/easytask/MainActivity.java
+++ app/src/main/java/com/zhangjie/easytask/MainActivity.java
@@ -29,7 +29,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private boolean isEnabled=true;
+    private boolean isEnabled = true;
     private SharedPreferences setting;
 
     @Override
@@ -39,69 +39,42 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final Button start= (Button) findViewById(R.id.start_point);
-        Button stop= (Button) findViewById(R.id.stop_point);
-        Button set_vibrate= (Button) findViewById(R.id.set_vibrate);
-        Button set_alpha= (Button) findViewById(R.id.set_alpha);
-        Button set_size= (Button) findViewById(R.id.set_size);
-        final LinearLayout tip= (LinearLayout) findViewById(R.id.tip);
-        //checkService();
+        final Button start = (Button) findViewById(R.id.start_point);
+        Button stop = (Button) findViewById(R.id.stop_point);
+        final LinearLayout tip = (LinearLayout) findViewById(R.id.tip);
 
-        if(isEnabled){tip.setVisibility(View.VISIBLE);}
+
+        if (isEnabled) {
+            tip.setVisibility(View.VISIBLE);
+        }
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (isEnabled) {
-                    Intent intent = new Intent(MainActivity.this, EasyTask.class);
-                    startService(intent);
-                    tip.setVisibility(View.VISIBLE);
-                } else {
-                    showDialog(MainActivity.this, "激活圆点", "您还没有激活圆点。" + "在设置中：系统 → 辅助功能 → 服务 中激活" + getResources().getString(R.string.app_name)
-                            + "后，便可使用圆点", "去激活", "取消");
-                }
+                Intent intent = new Intent(MainActivity.this, EasyTask.class);
+                startService(intent);
+                tip.setVisibility(View.VISIBLE);
             }
         });
         stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (isEnabled) {
-                    showDialog(MainActivity.this, "取消激活圆点", "您激活了圆点。" + "在设置中：系统 → 辅助功能 → 服务 中取消激活" + getResources().getString(R.string.app_name)
-                            + "后，便可停止使用圆点", "去关闭激活", "取消");
-                } else {
-                    Intent intent = new Intent(MainActivity.this, EasyTask.class);
-                    stopService(intent);
-                    finish();
-                }
+                Intent intent = new Intent(MainActivity.this, EasyTask.class);
+                stopService(intent);
+                finish();
+            }
+        });
 
-            }
-        });
-        set_vibrate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showSetDialog(MainActivity.this, "设置震动", 0);
-            }
-        });
-        set_alpha.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showSetDialog(MainActivity.this,"设置透明度",1);
-            }
-        });
-        set_size.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showSetDialog(MainActivity.this,"设置圆点大小",2);
-            }
-        });
-        if(Build.VERSION.SDK_INT>22&&!Settings.canDrawOverlays(this)){
+        if (Build.VERSION.SDK_INT > 22 && !Settings.canDrawOverlays(this)) {
             requestAlertWindowPermission();
         }
-        setting=getSharedPreferences("setting",MODE_PRIVATE);
+        setting = getSharedPreferences("setting", MODE_PRIVATE);
 
     }
+
     //6.0权限需要
     private static final int REQUEST_CODE = 1;
-    private  void requestAlertWindowPermission() {
+
+    private void requestAlertWindowPermission() {
         Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
         intent.setData(Uri.parse("package:" + getPackageName()));
         startActivityForResult(intent, REQUEST_CODE);
@@ -111,9 +84,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_CODE) {
-            if(Build.VERSION.SDK_INT>22){
+            if (Build.VERSION.SDK_INT > 22) {
                 if (Settings.canDrawOverlays(this)) {
-                    Toast.makeText(this,"已为6.0设备获得相关权限",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "已为6.0设备获得相关权限", Toast.LENGTH_SHORT).show();
                 }
             }
 
@@ -135,103 +108,10 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void showDialog(Context mContext, String title, String msg, String positiveMsg, String cancelMsg) {
-
-        AlertDialog.Builder builder;
-        builder = new AlertDialog.Builder(mContext);
-
-        builder.setTitle(title)
-                .setPositiveButton(positiveMsg, new positiveListener())
-                .setNegativeButton(cancelMsg, null)
-                .setCancelable(false);// 设置点击空白处，不能消除该对话框
-
-        builder.setMessage(msg).create().show();
-    }
-
-    public void showSetDialog(Context mContext,String title, final int type){
-        final AlertDialog.Builder builder= new AlertDialog.Builder(mContext);
-
-        LayoutInflater inflater= (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
-        View setLayout=inflater.inflate(R.layout.setting_dialog, (ViewGroup) findViewById(R.id.my_setting_dialog));
-
-        builder.setView(setLayout);
-        builder.setCancelable(false);
-        builder.setTitle(title);
-        SeekBar set_bar= (SeekBar) setLayout.findViewById(R.id.set_bar);
-        final TextView seek_value= (TextView) setLayout.findViewById(R.id.seek_value);
-        builder.setNegativeButton("取消", null)
-                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        switch (type) {
-                            case 0:
-                                MyWindowManager.updateEasyPoint(MainActivity.this, setting.getInt("vibrate", 0) + 1, 0, 0);
-                                break;
-                            case 1:
-                                MyWindowManager.updateEasyPoint(MainActivity.this, 0, setting.getInt("alpha", 0), 0);
-                                break;
-                            case 2:
-                                MyWindowManager.updateEasyPoint(MainActivity.this, 0, 0, setting.getInt("size", 0));
-                                break;
-                            default:
-                                break;
-                        }
-                    }
-                });
-        switch (type){
-            case 0:
-                set_bar.setProgress(setting.getInt("vibrate", 0));
-                seek_value.setText(""+setting.getInt("vibrate", 0));
-                break;
-            case 1:
-                set_bar.setProgress(setting.getInt("alpha", 100)/2);
-                seek_value.setText(""+setting.getInt("alpha", 100)/2);
-                break;
-            case 2:
-                set_bar.setProgress(setting.getInt("size", 50));
-                seek_value.setText(""+setting.getInt("size", 50));
-                break;
-            default:
-                break;
-        }
-        set_bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                seek_value.setText("" + i);
-                switch (type) {
-                    case 0:
-                        setting.edit().putInt("vibrate", i).commit();
-                        break;
-                    case 1:
-                        setting.edit().putInt("alpha", i * 2).commit();
-                        break;
-                    case 2:
-                        setting.edit().putInt("size", i).commit();
-                        break;
-                    default:
-                        break;
-                }
-
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
-        builder.create().show();
-    }
 
     @Override
     protected void onResume() {
         super.onResume();
-        //checkService();
     }
 
     private class positiveListener implements DialogInterface.OnClickListener {
@@ -241,17 +121,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-    public void checkService(){
-        AccessibilityManager manager = (AccessibilityManager) getSystemService(ACCESSIBILITY_SERVICE);
-        List<AccessibilityServiceInfo> list = AccessibilityManagerCompat.getEnabledAccessibilityServiceList(manager,
-                AccessibilityServiceInfo.FEEDBACK_ALL_MASK);
-        if (list.size()==0) isEnabled=false;
-        for (int i = 0; i < list.size(); i++) {
-            if ("com.zhangjie.easytask/.EasyTask".equals(list.get(i).getId())) {
-                isEnabled = true;
-                break;
-            }
-        }
-    }
 }
